2f57b1dad5900f7f9e58ac6b7b086fd26b6ce1c4
NHI <khainhi03@gmail.com>
2024-12-12 20:48:21 +0700

    Tạo class SongNode

--- SongNode.cs
+++ SongNode.cs
@@ -8,5 +8,21 @@ namespace MusicPlayer
 {
     internal class SongNode
     {
+        public string FilePath { get; set; } // Đường dẫn bài hát
+        public string FileName { get; set; } // Tên bài hát
+        public SongNode Next { get; set; }   // Node tiếp theo
+        public SongNode Previous { get; set; } // Node trước đó
+
+        public SongNode(string filePath, string fileName)
+        {
+            FilePath = filePath;
+            FileName = fileName;
+            Next = null;
+            Previous = null;
+        }
+        public override string ToString()
+        {
+            return FileName;
+        }
     }
 }
